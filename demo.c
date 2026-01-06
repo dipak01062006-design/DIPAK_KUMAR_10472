53338bf722d4816b03a660c50a71ebc108525a4d
dipak01062006-design <dipak01062006@gmail.com>
2026-01-06 20:58:10 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -5,34 +5,26 @@ int main() {
     char password[50];
     int i;
     int Digit = 0, Upper = 0;
-
     printf("Enter your password:\n ");
     scanf("%s", password);
-
-    // Check length
     if (strlen(password) < 8) {
         printf("Weak Password :\n");
         printf("Reason: Password must be at least 8 characters long.\n");
         return 0;
     }
-
-    // Check each character
     for (i = 0; password[i] != '\0'; i++) {
         if (isdigit(password[i]))
             Digit = 1;
         if (isupper(password[i]))
             Upper = 1;
     }
-
-    // Final check
     if (Digit && Upper) {
         printf("Strong Password \n");
     } else {
         printf("Weak Password \n");
         printf("Reason: Password must contain at least one digit and one uppercase letter.\n");
     }
-
     return 0;
 }
-i want to add the output of this code in my readme file but in screenshot form
+
 
